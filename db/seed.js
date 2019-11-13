953f8723bab6e6958830c7b5318aa4d83761e0e8
Jenna Dean <jenna.dean9@gmail.com>
2019-11-13 12:33:51 -0500

    seeding done

--- db/seed.js
+++ db/seed.js
@@ -1,18 +1,8 @@
 const Index = require("../models/stockIndex.js");
 const data = require("./stockIndex.json");
 
-const stockData = data.map(item => {
-  const company = {};
-  company.name = item.name;
-  company.symbol = item.symbol;
-  company.sector = item.sector;
-  return company;
-});
-
-console.log(stockData);
-
 Index.remove({}).then(() => {
-  Index.create(stockData)
+  Index.create(data)
     .then(index => {
       console.log(index);
       process.exit();
